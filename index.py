f99d08b53710de7f9dfc1d1e8e506c9d0f572ca7
SandIce <37013075+feizao67@users.noreply.github.com>
2021-05-06 21:01:12 +0800

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
             name.append(info[0])
             loginToken.append(info[1])
             studentID.append(info[2])
-            # checkinfo[name] = [loginToken, studentID]
+            checkinfo[name] = [loginToken, studentID]
         except:
             break
-    print(name)
+    print(checkinfo)
